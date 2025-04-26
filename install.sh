adb214c7da4829a282f58cc5f860c6ab52cadc30
Alexander Gent <acegent2907@optusnet.com.au>
2025-04-26 14:26:48 +1000

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,11 +2,57 @@
 
 currentuser=$(users | awk '{print $1}')
 
+function run-in-user-session() {
+    _display_id=":$(find /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+    _username=$(who | grep "($_display_id)" | awk '{print $1}')
+    _user_id=$(id -u "$_username")
+    _environment=("DISPLAY=$_display_id" "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$_user_id/bus")
+    sudo -Hu "$_username" env "${_environment[@]}" "$@"
+}
+
+function desktop-settings() {
+echo -e '\033[1;33mUpdating   \033[1;34mDesktop Themes And Settings...\033[0m'
+# Desktop Icon Settings
+run-in-user-session dconf write /org/nemo/desktop/computer-icon-visible "true"
+run-in-user-session dconf write /org/nemo/desktop/home-icon-visible "true"
+run-in-user-session dconf write /org/nemo/desktop/network-icon-visible "true"
+run-in-user-session dconf write /org/nemo/desktop/show-orphaned-desktop-icons "false"
+run-in-user-session dconf write /org/nemo/desktop/trash-icon-visible "true"
+run-in-user-session dconf write /org/nemo/desktop/volumes-visible "false"
+# Desktop Background Settings
+run-in-user-session dconf write /org/cinnamon/desktop/background/slideshow/delay 5
+run-in-user-session dconf write /org/cinnamon/desktop/background/slideshow/slideshow-enabled "true"
+run-in-user-session dconf write /org/cinnamon/desktop/background/slideshow/random-order "true"
+run-in-user-session dconf write /org/cinnamon/desktop/background/slideshow/image-source "'xml:///usr/share/cinnamon-background-properties/linuxmint-wilma.xml'"
+#Screen Saver
+run-in-user-session dconf write /org/cinnamon/desktop/session/idle-delay "uint32 0"
+run-in-user-session dconf write /org/cinnamon/desktop/screensaver/lock-enabled "false"
+run-in-user-session dconf write /org/cinnamon/desktop/screensaver/show-notifications "false"
+# Power Management
+run-in-user-session dconf write /org/cinnamon/settings-daemon/plugins/power/sleep-display-ac 0
+run-in-user-session dconf write /org/cinnamon/settings-daemon/plugins/power/sleep-inactive-ac-timeout 0
+run-in-user-session dconf write /org/cinnamon/settings-daemon/plugins/power/button-power "'shutdown'"
+run-in-user-session dconf write /org/cinnamon/settings-daemon/plugins/power/lock-on-suspend "false"
+# Themes
+run-in-user-session dconf write /org/cinnamon/desktop/wm/preferences/theme "'Mint-Y-Dark'"
+run-in-user-session dconf write /org/cinnamon/desktop/wm/preferences/theme-backup "'Mint-Y-Dark'"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/icon-theme "'hi-color'"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/icon-theme-backup "'hi-color'"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/gtk-theme "'Adwaita-dark'"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/gtk-theme-backup "'Adwaita-dark'"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/cursor-theme "'DMZ-White'"
+run-in-user-session dconf write /org/cinnamon/theme/name "'Adapta-Nokto'"
+# Time / Date
+run-in-user-session dconf write /org/cinnamon/desktop/interface/clock-show-date "true"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/clock-show-seconds "true"
+run-in-user-session dconf write /org/cinnamon/desktop/interface/clock-use-24h "false"
+}
+
 function get-conky() {
 echo -e '\033[1;33mInstalling \033[1;34mConky\033[0m'
 apt-get -y -qq install conky-all >/dev/null
+apt-get -y -qq install lsscsi >/dev/null
 cp -f conky.conf /home/$currentuser/.conkyrc
-chmod 0777 -R /home/$currentuser/.conkyrc
 echo -e '[Desktop Entry]'>/home/$currentuser/.config/autostart/conky.desktop
 echo -e 'Type=Application'>>/home/$currentuser/.config/autostart/conky.desktop
 echo -e 'Exec=/usr/bin/conky -d'>>/home/$currentuser/.config/autostart/conky.desktop
@@ -17,10 +63,11 @@ echo -e 'Name[en_AU]=Conky'>>/home/$currentuser/.config/autostart/conky.desktop
 echo -e 'Comment[en_AU]=System information tool'>>/home/$currentuser/.config/autostart/conky.desktop
 echo -e 'X-GNOME-Autostart-Delay=5'>>/home/$currentuser/.config/autostart/conky.desktop
 ## HDSentinel
-wget -q -O /tmp/hdsentinel.gz https://www.hdsentinel.com/hdslin/hdsentinel-019c-x64.gz
-gunzip -d /tmp/hdsentinel.gz
-mv -f /tmp/hdsentinel /bin
-chmod 0777 /bin/hdsentinel
+wget -q -O /tmp/hdsentinel.zip https://www.hdsentinel.com/hdslin/hdsentinel-020c-x64.zip
+unzip -oq /tmp/hdsentinel.zip -d /tmp
+mv -f /tmp/HDSentinel /bin/hdsentinel
+rm -f /tmp/hdsentinel.zip
+chmod 0777 -f /bin/hdsentinel
 if ! grep -Fxq $currentuser" ALL=NOPASSWD: /bin/hdsentinel" /etc/sudoers
 then
     echo $currentuser" ALL=NOPASSWD: /bin/hdsentinel">>/etc/sudoers
@@ -35,4 +82,9 @@ then
 fi
 }
 
+## START INSTALLATION ##
+history -c
+reset
+
 get-conky
+desktop-settings
